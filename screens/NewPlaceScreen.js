7a34bc29948400d70e80fdebb664b7c4f51fb4c8
bastej <siatkasebastian@gmail.com>
2020-02-05 16:06:53 +0100

    Creating create place basic form

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,18 +1,48 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, ScrollView, Button, TextInput } from "react-native";
+
+import ColorPalette from "../constants/ColorPalette";
 
 const NewPlaceScreen = props => {
+  const [titleValue, setTitleValue] = useState("");
+
+  const onTitleChange = value => {
+    setTitleValue(value);
+  };
+
+  const handleCreatePlace = () => {};
+
   return (
-    <View>
-      <Text>New Place Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput style={styles.input} onChangeText={onTitleChange} value={titleValue} />
+        <Button
+          style={styles.submitButton}
+          title="Add Place"
+          color={ColorPalette.green}
+          onPress={handleCreatePlace}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
 NewPlaceScreen.navigationOptions = {
-  headerTitle: "Add Place"
+  headerTitle: "Add Place",
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: { margin: 30 },
+  label: { fontSize: 18, marginBottom: 15 },
+  input: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+  submitButton: {},
+});
 
 export default NewPlaceScreen;
