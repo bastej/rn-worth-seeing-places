aa4a8d46ea229a5720ac595437b065d6d1991e71
bastej <siatkasebastian@gmail.com>
2020-02-07 12:00:15 +0100

    Division env file into dev and prod parts

--- env.js
+++ env.js
@@ -1,3 +1,17 @@
-export default {
-  googleApiKey: "AIzaSyDIkCvDsASBHqtbP7lahJ-8Us2rh-vVu9g",
+const variables = {
+  development: {
+    googleApiKey: "AIzaSyDIkCvDsASBHqtbP7lahJ-8Us2rh-vVu9g",
+  },
+  production: {
+    googleApiKey: "test",
+  },
 };
+
+const getEnvVariables = () => {
+  if (__DEV__) {
+    return variables.development; // return this if in development mode
+  }
+  return variables.production; // otherwise, return this
+};
+
+export default getEnvVariables; // export a reference to the function
