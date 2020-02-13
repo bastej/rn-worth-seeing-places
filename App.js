2ec5dad1352c50c816fd98d993569ac0e3fa54cd
bastej <siatkasebastian@gmail.com>
2020-02-13 20:44:43 +0100

    Adding custom fonts and Text Comp.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 
 import { createStore, applyMiddleware } from "redux";
@@ -6,10 +6,13 @@ import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+
 import { NavigationContainer } from "@react-navigation/native";
+import { PlacesNavigator } from "./navigation/PlacesNavigator";
 
 import rootReducer from "./store/reducers";
-
 import { init } from "./helpers/db";
 
 init()
@@ -21,11 +24,22 @@ init()
     console.log(err);
   });
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
-import { PlacesNavigator } from "./navigation/PlacesNavigator";
-
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />;
+  }
+
   return (
     <Provider store={store}>
       <NavigationContainer>
